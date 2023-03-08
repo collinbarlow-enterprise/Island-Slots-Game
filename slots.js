b2ddbd29a8ae95bfaf1a2a82a8aa59bc88f92545
CollinBarlow <collinbarlow@gmail.com>
2023-03-08 15:20:36 -0800

    animating Slots

--- slots.js
+++ slots.js
@@ -25,12 +25,14 @@ let resetBtnEl = document.getElementById("reset");
 let spin1El = document.getElementById("spin1");
 let spin2El = document.getElementById("spin2");
 let spin3El = document.getElementById("spin3");
+let spinAllEl = document.querySelectorAll(".spinners") //should be able to get rid of spin1el, etc, and replace with a forEach loop
 
 
 //need to code an onclick evt for each of these that update the wager value with their specified value
 let wagerBtn10 = document.getElementById("wager1")
 let wagerBtn20 = document.getElementById("wager2")
 let wagerBtn50 = document.getElementById("wager3")
+// let wagerBtnAllEl = document.getElementsByClassName("bets") // shoud be able to get rid of wagerBtn, etc, and replace with a forEach loop
 
  //need to put this in a function
 let purseEl = document.getElementById("purse");
@@ -46,6 +48,8 @@ let winSoundAudio = document.getElementById("winSound");
 wagerBtn10.addEventListener('click', setWager);
 wagerBtn20.addEventListener('click', setWager);
 wagerBtn50.addEventListener('click', setWager);
+// wagerBtnAllEl.addEventListener('click', setWager); //should be able to replace these event listeners with wagerBtnAllEl now
+
 
 // wagerBtn10.addEventListener('click', updateWagerEl);
 // wagerBtn20.addEventListener('click', updateWagerEl);
@@ -74,7 +78,9 @@ function render() {
     //had setWager here but think render should be nested inside setWager?
     // setWager();
     // updateWagerEl();//only updates on play being clicked - need to add evt listener and set it seperate
-    getSpinner();
+    animateSlots();
+    void spinAllEl.offsetHeight;
+    // getSpinner();
     updatePurse();
     updateWinnings();
     endGame();
@@ -100,23 +106,34 @@ function getSpinner3() {
     return fruits3[rndIdx];
 };
 
+function animateSlots() {
+    spinAllEl.forEach((slot) => {
+    slot.style.animation ;})
+    setTimeout(getSpinner, 2000);
+    void spinAllEl.offsetHeight;
+}
+
 //call and cache 3 spinner functions
 //compare each spin and return positive value to purse
 //if no winner subtract the value of wager from purse
 function getSpinner() {
     let spin1 = getSpinner1();
+    // animateSlots();
     document.getElementById("spin1").src = `${SPINNERS[spin1].img}`; 
     let spin2 = getSpinner2();
     document.getElementById("spin2").src = `${SPINNERS[spin2].img}`; 
     let spin3 = getSpinner3();
     document.getElementById("spin3").src = `${SPINNERS[spin3].img}`; 
     if (spin1 === "banana" && spin1 === spin2 && spin1 === spin3) {
+        coinSoundAudio.volume = 0.2; 
         coinSoundAudio.play();
         return purse = wager*10 + purse;
     } else if (spin1 === "mango" && spin1 === spin2 && spin1 === spin3) {
+        coinSoundAudio.volume = 0.2; 
         coinSoundAudio.play();
         return purse = wager*7 + purse;
     } else if (spin1 === "pineapple" && spin1 === spin2 && spin1 === spin3) {
+        coinSoundAudio.volume = 0.2; 
         coinSoundAudio.play();
         return purse = wager*5 + purse;
     } else {
@@ -127,12 +144,14 @@ function getSpinner() {
 // evaluates what to do when a endGame condition is met
 function endGame(){
     if (purse <= 0) {
+        loseSoundAudio.volume = 0.1;
         loseSoundAudio.play();
         winLoseEl.innerText = "YOU LOSE";
         winLoseEl.style.visibility = "visible";
         playBtnEl.style.visibility = "hidden";
         resetBtnEl.style.visibility = "visible";
     } else if (purse >= 2000) {
+        winSoundAudio.volume = 0.2;
         winSoundAudio.play();
         winLoseEl.innerText = "YOU WIN";
         winLoseEl.style.visibility = "visible";
@@ -152,9 +171,14 @@ function updatePurse() {
 };
 
 function setWager(evt) {
+// wagerBtnAllEl.forEach((button)=> {
     wager = evt.target.innerText;
     updateWagerEl();
-}
+};
+
+    // wager = evt.target.innerText; need to use a foreach
+    // updateWagerEl();
+
 
 function updateWinnings() {
    winningsEl.innerHTML = `WINNINGS: ${purse-1000}`;
